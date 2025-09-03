767f5cac6609e8967dcb22f1fda204c62c21b3ee
허예진 <yejin097@naver.com>
2025-09-03 22:45:30 +0900

    feat: 웨딩 사진 반영

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -10,6 +10,6 @@ export const LOCATION = {
 export const LINK = {
   NAVER_MAP: `https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${aiNaverApiClientId}`,
   NAVER_MAP_APP: `nmap://place?lat=${LOCATION.LATITUDE}&lng=${LOCATION.LONGITUDE}&name=${LOCATION.NAME}&appname=localhost:4321`,
-  KAKAO_MAP_APP: `https://map.kakao.com/link/to/${LOCATION.NAME},${LOCATION.LATITUDE},${LOCATION.LONGITUDE}`,
+  KAKAO_MAP_APP: `https://map.kakao.com/link/to/${encodeURIComponent(LOCATION.NAME)},${LOCATION.LATITUDE},${LOCATION.LONGITUDE}`,
   T_MAP_APP: `https://apis.openapi.sk.com/tmap/app/map?appKey=${tMapAppKey}&name=${LOCATION.NAME}&lon=${LOCATION.LONGITUDE}&lat=${LOCATION.LATITUDE}`,
 };
